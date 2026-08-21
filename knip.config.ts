24bb811778a2699d38e671d2d99c7539cb1bf5b5
Leonardo Araujo <leonardo.sd.araujo@totalpass.com.br>
2026-08-21 08:58:03 -0300

    chore: remove e2e tests

--- knip.config.ts
+++ knip.config.ts
@@ -14,6 +14,7 @@ const config: KnipConfig = {
     '@hookform/resolvers',
     'react-hook-form',
     'vitest-browser-react',
+    '@faker-js/faker',
     '@swc/helpers', // Avoid error in CI: "`npm ci` can only install packages when your package.json and package-lock.json or npm-shrinkwrap.json are in sync."
   ],
   // Include custom Playwright test file suffixes
